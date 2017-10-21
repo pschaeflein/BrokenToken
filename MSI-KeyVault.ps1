f15185ff9b1361cb5bbb4f4424695258c15c6d45
unknown <paul@schaeflein.net>
2017-10-21 14:23:28 -0500

    SPS SAC demos

---
+++ MSI-KeyVault.ps1
@@ -0,0 +1,9 @@
+﻿# https://docs.microsoft.com/en-us/azure/active-directory/msi-tutorial-windows-vm-access-nonaad
+
+$response = Invoke-WebRequest -Uri http://localhost:50342/oauth2/token -Method GET -Body @{resource="https://vault.azure.net"} -Headers @{Metadata="true"}
+$content = $response.Content | ConvertFrom-Json
+$KeyVaultToken = $content.access_token
+$KeyVaultSecret = (Invoke-WebRequest -Uri https://aad-dev-vault.vault.azure.net/secrets/LiveDemo?api-version=2016-10-01 -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).content | ConvertFrom-Json
+$secret = $KeyVaultSecret.value
+
+Write-Host $secret
---
+++ PnP-CredMgr.ps1
@@ -0,0 +1,7 @@
+﻿# https://github.com/SharePoint/PnP-PowerShell/wiki/How-to-use-the-Windows-Credential-Manager-to-ease-authentication-with-PnP-PowerShell
+
+Connect-PnPOnline -Url https://scdevtenant.sharepoint.com -Credentials FirstTest
+
+$title = (Get-PnPWeb).title
+
+Write-Host $title
